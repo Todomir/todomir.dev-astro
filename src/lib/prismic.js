2be5fc2187a5095a63e75ac4b914ec379da492af
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2021-10-14 20:16:12 -0300

    feat(lib): add prismic lib

---
+++ src/lib/prismic.js
@@ -0,0 +1,33 @@
+import Prismic from '@prismicio/client';
+
+const { PRISMIC_API_URL } = process.env;
+
+const client = Prismic.client(PRISMIC_API_URL);
+
+export async function getPageByType(slug, lang) {
+  const response = await client.query(
+    Prismic.Predicates.at('document.type', slug),
+    { lang }
+  )
+  return response;
+}
+
+export async function getPageByUID(type, slug, lang) {
+  const response = await client.getByUID(type, slug, { lang });
+  return response;
+}
+
+export async function getAllFormattedLanguages() {
+  const response = await fetch(PRISMIC_API_URL)
+  const json = await response.json()
+  return json.languages.map(lang => {
+    const isIdHyphenated = lang.id.includes('-')
+    if (isIdHyphenated) {
+      const splitted = lang.id.split('-')
+			const language = splitted[0] + '-' + splitted[1].toUpperCase()
+			return language
+    } else {
+      return lang.id
+    }
+  })
+}
