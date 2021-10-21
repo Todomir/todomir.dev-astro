64fb4be1e0fea5c3460ef8309e15338d59abd433
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2021-10-21 17:22:17 -0300

    style(prettier): lint files with prettier

--- src/lib/prismic.js
+++ src/lib/prismic.js
@@ -1,85 +1,85 @@
-import Prismic from "@prismicio/client";
-import PrismicDOM from "prismic-dom";
+import Prismic from '@prismicio/client'
+import PrismicDOM from 'prismic-dom'
 
-const { Elements } = PrismicDOM.RichText;
+const { Elements } = PrismicDOM.RichText
 
-const { PRISMIC_API_URL } = process.env;
+const { PRISMIC_API_URL } = process.env
 
-const client = Prismic.client(PRISMIC_API_URL);
+const client = Prismic.client(PRISMIC_API_URL)
 
 export function serializer(type, element, content, children, classes) {
   switch (type) {
     case Elements.heading1:
       return `<h1 ${classes.h1 && `class=${classes.h1}`}>${children.join(
-        ""
-      )}</h1>`;
+        ''
+      )}</h1>`
     case Elements.heading2:
       return `<h2 ${classes.h2 && `class=${classes.h2}`}>${children.join(
-        ""
-      )}</h2>`;
+        ''
+      )}</h2>`
     case Elements.heading3:
       return `<h3 ${classes.h3 && `class=${classes.h3}`}>${children.join(
-        ""
-      )}</h3>`;
+        ''
+      )}</h3>`
     case Elements.heading4:
       return `<h4 ${classes.h4 && `class=${classes.h4}`}>${children.join(
-        ""
-      )}</h4>`;
+        ''
+      )}</h4>`
     case Elements.heading5:
       return `<h5 ${classes.h5 && `class=${classes.h5}`}>${children.join(
-        ""
-      )}</h5>`;
+        ''
+      )}</h5>`
     case Elements.heading6:
       return `<h6 ${classes.h6 && `class=${classes.h6}`}>${children.join(
-        ""
-      )}</h6>`;
+        ''
+      )}</h6>`
     case Elements.paragraph:
-      return `<p ${classes.p && `class=${classes.p}`}>${children.join("")}</p>`;
+      return `<p ${classes.p && `class=${classes.p}`}>${children.join('')}</p>`
     case Elements.preformatted:
       return `<pre ${classes.pre && `class=${classes.pre}`}>${children.join(
-        ""
-      )}</pre>`;
+        ''
+      )}</pre>`
     case Elements.strong:
       return `<strong ${
         classes.strong && `class=${classes.strong}`
-      }>${children.join("")}</strong>`;
+      }>${children.join('')}</strong>`
     case Elements.em:
       return `<em ${classes.em && `class=${classes.em}`}>${children.join(
-        ""
-      )}</em>`;
+        ''
+      )}</em>`
     case Elements.listItem:
       return `<li ${classes.li && `class=${classes.li}`}>${children.join(
-        ""
-      )}</li>`;
+        ''
+      )}</li>`
     case Elements.oListItem:
       return `<li ${classes.li && `class=${classes.li}`}>${children.join(
-        ""
-      )}</li>`;
+        ''
+      )}</li>`
     case Elements.list:
       return `<li ${classes.li && `class=${classes.li}`}>${children.join(
-        ""
-      )}</li>`;
+        ''
+      )}</li>`
     case Elements.oList:
       return `<ol ${classes.ol && `class=${classes.ol}`}>${children.join(
-        ""
-      )}</ol>`;
+        ''
+      )}</ol>`
     case Elements.image:
       var linkUrl = element.linkTo
         ? PrismicDOM.Link.url(element.linkTo, module.exports.linkResolver)
-        : null;
+        : null
       var linkTarget =
         element.linkTo && element.linkTo.target
           ? `target="${element.linkTo.target}" rel="noopener"`
-          : "";
-      var wrapperClassList = [element.label || "", "block-img"];
+          : ''
+      var wrapperClassList = [element.label || '', 'block-img']
       var img = `<img src="${element.url}" alt="${
-        element.alt || ""
-      }" copyright="${element.copyright || ""}">`;
+        element.alt || ''
+      }" copyright="${element.copyright || ''}">`
       return `
-        <p class="${wrapperClassList.join(" ")}">
+        <p class="${wrapperClassList.join(' ')}">
           ${linkUrl ? `<a ${linkTarget} href="${linkUrl}">${img}</a>` : img}
         </p>
-      `;
+      `
     case Elements.embed:
       return `
         <div data-oembed="${element.oembed.embed_url}"
@@ -89,60 +89,60 @@ export function serializer(type, element, content, children, classes) {
         >
           ${element.oembed.html}
         </div>
-      `;
+      `
     case Elements.hyperlink:
       var target = element.data.target
         ? `target="${element.data.target}" rel="noopener"`
-        : "";
-      var linkUrl = PrismicDOM.Link.url(element.data, linkResolver);
+        : ''
+      var linkUrl = PrismicDOM.Link.url(element.data, linkResolver)
       return `<a ${target} href="${linkUrl}" ${
         classes.a && `class=${classes.a}`
-      }>${children.join("")}</a>`;
+      }>${children.join('')}</a>`
     case Elements.label:
       var label = element.data.label
         ? ` class="${element.data.label} ${classes.label && classes.label}"`
-        : "";
-      return `<span ${label}>${children.join("")}</span>`;
+        : ''
+      return `<span ${label}>${children.join('')}</span>`
     case Elements.span:
-      return content ? content.replace(/\n/g, "<br />") : "";
+      return content ? content.replace(/\n/g, '<br />') : ''
     default:
-      return null;
+      return null
   }
 }
 
 export async function getPageByType(slug, lang) {
   const response = await client.query(
-    Prismic.Predicates.at("document.type", slug),
+    Prismic.Predicates.at('document.type', slug),
     { lang }
-  );
-  return response;
+  )
+  return response
 }
 
 export async function getPageByUID(type, slug, options) {
-  const response = await client.getByUID(type, slug, options);
-  return response;
+  const response = await client.getByUID(type, slug, options)
+  return response
 }
 
 export async function getDataByType(type, options) {
   const response = await client.query(
-    Prismic.Predicates.at("document.type", type),
+    Prismic.Predicates.at('document.type', type),
     options
-  );
+  )
 
-  return response;
+  return response
 }
 
 export async function getAllFormattedLanguages() {
-  const response = await fetch(PRISMIC_API_URL);
-  const json = await response.json();
-  return json.languages.map((lang) => {
-    const isIdHyphenated = lang.id.includes("-");
+  const response = await fetch(PRISMIC_API_URL)
+  const json = await response.json()
+  return json.languages.map(lang => {
+    const isIdHyphenated = lang.id.includes('-')
     if (isIdHyphenated) {
-      const splitted = lang.id.split("-");
-      const language = splitted[0] + "-" + splitted[1].toUpperCase();
-      return language;
+      const splitted = lang.id.split('-')
+      const language = splitted[0] + '-' + splitted[1].toUpperCase()
+      return language
     } else {
-      return lang.id;
+      return lang.id
     }
-  });
+  })
 }
