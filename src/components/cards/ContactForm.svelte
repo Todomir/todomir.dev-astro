03435d23ad8fca81b83724cca8439ff636627964
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2021-10-24 11:37:42 -0300

    fix(components/contact-form): fix uneven spacing

--- src/components/cards/ContactForm.svelte
+++ src/components/cards/ContactForm.svelte
@@ -89,77 +89,85 @@
   }
 </script>
 
-<section id="contact">
-  <h1>{prismicDom.RichText.asText(slice.data.title)}</h1>
-  <p>{prismicDom.RichText.asText(slice.data.description)}</p>
-  <Form {onSubmit} {recaptchaKey}>
-    {#each fields as field}
-      {#if field.field === 'input'}
-        <Input
-          required
-          type={field.type}
-          name={field.name}
-          label={field.label}
-          placeholder={field.placeholder}
-          icon={field.icon}
-        />
-      {:else if field.field === 'textarea'}
-        <Textarea
-          required
-          rows="5"
-          name={field.name}
-          label={field.label}
-          placeholder={field.placeholder}
-        />
-      {:else}
-        Field does not exist
-      {/if}
-    {/each}
-    <button class={`btn btn-dark ${formState} g-recaptcha`}>
-      {#if formState === 'loading'}
-        {slice.data.cta_loading}
-      {:else if formState === 'success'}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+<div class="container">
+
+  <section id="contact">
+    <h1>{prismicDom.RichText.asText(slice.data.title)}</h1>
+    <p>{prismicDom.RichText.asText(slice.data.description)}</p>
+    <Form {onSubmit} {recaptchaKey}>
+      {#each fields as field}
+        {#if field.field === 'input'}
+          <Input
+            required
+            type={field.type}
+            name={field.name}
+            label={field.label}
+            placeholder={field.placeholder}
+            icon={field.icon}
           />
-        </svg>
-        {slice.data.cta_success}
-      {:else if formState === 'error'}
-        {slice.data.cta_error}
-      {:else}
-        {slice.data.cta_label}
-      {/if}
-    </button>
-  </Form>
-</section>
+        {:else if field.field === 'textarea'}
+          <Textarea
+            required
+            rows="5"
+            name={field.name}
+            label={field.label}
+            placeholder={field.placeholder}
+          />
+        {:else}
+          Field does not exist
+        {/if}
+      {/each}
+      <button class={`btn btn-dark ${formState} g-recaptcha`}>
+        {#if formState === 'loading'}
+          {slice.data.cta_loading}
+        {:else if formState === 'success'}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          {slice.data.cta_success}
+        {:else if formState === 'error'}
+          {slice.data.cta_error}
+        {:else}
+          {slice.data.cta_label}
+        {/if}
+      </button>
+    </Form>
+  </section>
+</div>
 
 <style lang="scss">
+  .container {
+    padding-inline: 24px;
+  }
+
   section {
     --padding-inline: clamp(4.4rem, 3.0805rem + 3.1873vw, 9.2rem);
 
     background-color: var(--color-white);
     border-radius: 30px;
 
-    padding-block: 100px;
+    padding-block: 64px;
     padding-inline: var(--padding-inline);
 
     max-width: 95.3rem;
     margin: 0 auto;
 
     @media screen and (max-width: 768px) {
-      width: calc(80% + (var(--body-padding) * 2));
-      margin-inline: calc(-0.7 * var(--body-padding));
+      width: calc(100% + (var(--body-padding) * 2));
+      margin-left: calc(-1 * var(--body-padding));
+      padding-block: 100px;
     }
 
     button {
